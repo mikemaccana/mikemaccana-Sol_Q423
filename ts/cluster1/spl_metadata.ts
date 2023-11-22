5178fd8b19d639f0c480cef8715bfedb6b5b1fc0
Mike MacCana <mike.maccana@gmail.com>
2023-11-21 21:54:19 -0500

    Use current JS, TS, esrun, make spl_init work

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -1,32 +1,47 @@
-import { Commitment, Connection, Keypair, PublicKey, Transaction, sendAndConfirmTransaction } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
-import { createCreateMetadataAccountV2Instruction, createCreateMetadataAccountV3Instruction } from "@metaplex-foundation/mpl-token-metadata";
+import {
+  Commitment,
+  Connection,
+  Keypair,
+  PublicKey,
+  Transaction,
+  sendAndConfirmTransaction,
+} from "@solana/web3.js";
+import { getKeypairFromEnvironment } from "@solana-developers/node-helpers";
+import dotenv from "dotenv";
+dotenv.config();
+
+import {
+  createCreateMetadataAccountV2Instruction,
+  createCreateMetadataAccountV3Instruction,
+} from "@metaplex-foundation/mpl-token-metadata";
 
 // We're going to import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const keypair = getKeypairFromEnvironment("DEV_WALLET");
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
 const connection = new Connection("https://api.devnet.solana.com", commitment);
 
-// Define our Mint address
-const mint = new PublicKey("<mint address>")
+// Define our Mint address (from spl_init)
+const mint = new PublicKey("FkaNBDjRjN32QSqzKaQNq4HXyZNvocdgJkksWRpMvGjg");
 
 // Add the Token Metadata Program
-const token_metadata_program_id = new PublicKey('metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s')
+const token_metadata_program_id = new PublicKey(
+  "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+);
 
 // Create PDA for token metadata
 const metadata_seeds = [
-    Buffer.from('metadata'),
-    token_metadata_program_id.toBuffer(),
-    mint.toBuffer(),
+  Buffer.from("metadata"),
+  token_metadata_program_id.toBuffer(),
+  mint.toBuffer(),
 ];
-const [metadata_pda, _bump] = PublicKey.findProgramAddressSync(metadata_seeds, token_metadata_program_id);
+const [metadata_pda, _bump] = PublicKey.findProgramAddressSync(
+  metadata_seeds,
+  token_metadata_program_id
+);
 
-(async () => {
-    try {
-        // Start here
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
-})();
+try {
+} catch (error) {
+  console.error(`Oops, something went wrong: ${error}`);
+}
--- ts/cluster1/spl_transfer.ts
+++ ts/cluster1/spl_transfer.ts
@@ -1,9 +1,18 @@
-import { Commitment, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js"
-import wallet from "../wba-wallet.json"
+import {
+  Commitment,
+  Connection,
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+} from "@solana/web3.js";
+import { getKeypairFromEnvironment } from "@solana-developers/node-helpers";
+import dotenv from "dotenv";
+dotenv.config();
+
 import { getOrCreateAssociatedTokenAccount, transfer } from "@solana/spl-token";
 
 // We're going to import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+const keypair = await getKeypairFromEnvironment("DEV_WALLET");
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
@@ -16,13 +25,11 @@ const mint = new PublicKey("<mint address>");
 const to = new PublicKey("<receiver address>");
 
 (async () => {
-    try {
-        // Get the token account of the fromWallet address, and if it does not exist, create it
-
-        // Get the token account of the toWallet address, and if it does not exist, create it
-
-        // Transfer the new token to the "toTokenAccount" we just created
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+  try {
+    // Get the token account of the fromWallet address, and if it does not exist, create it
+    // Get the token account of the toWallet address, and if it does not exist, create it
+    // Transfer the new token to the "toTokenAccount" we just created
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
